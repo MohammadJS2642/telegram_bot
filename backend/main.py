ff733194081a91515f506bdc263eca2c1f285dfb
MohammadJS2642 <mohammadjs2642@gmail.com>
2021-08-27 21:28:56 +0430

    .env and .env.example created

--- backend/main.py
+++ backend/main.py
@@ -1,2 +1,8 @@
 import logging
 from telegram.ext import Updater, CallbackContext, CommandHandler
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+logging.getLogger()
